481d8c18a75d85d4745e1ee6406b0a09390c9dc9
Thushan Madhushara <122750947+thushanm@users.noreply.github.com>
2025-08-05 23:22:54 +0530

    first commit

---
+++ Online-Billing-Symtem-Pahana-Edu/src/main/java/com/example/Online/Billing/Symtem/Pahana/Edu/OnlineBillingSymtemPahanaEduApplication.java
@@ -0,0 +1,13 @@
+package com.example.Online.Billing.Symtem.Pahana.Edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBillingSymtemPahanaEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBillingSymtemPahanaEduApplication.class, args);
+	}
+
+}
---
+++ Online-Billing-Symtem-Pahana-Edu/src/test/java/com/example/Online/Billing/Symtem/Pahana/Edu/OnlineBillingSymtemPahanaEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Online.Billing.Symtem.Pahana.Edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBillingSymtemPahanaEduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
