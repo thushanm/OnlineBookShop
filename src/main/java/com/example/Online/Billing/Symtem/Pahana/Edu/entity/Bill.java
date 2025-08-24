03e924e4ee1f44501b802d083bd3232d2c4ae6ab
Thushan Madhushara <122750947+thushanm@users.noreply.github.com>
2025-08-24 22:49:15 +0530

    Entity Added Bill And Item

---
+++ src/main/java/com/example/Online/Billing/Symtem/Pahana/Edu/entity/Bill.java
@@ -0,0 +1,24 @@
+package com.example.Online.Billing.Symtem.Pahana.Edu.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Bill {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+    private int unitsConsumed;
+    private double totalAmount;
+    private LocalDate billDate;
+}
